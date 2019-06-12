7209cbef5551a05d4590c13966d46989e13965a5
aokidaiki  <aoki.d0829@gmail.com>
2019-06-12 13:05:36 +0900

    first commit

---
+++ common/common.php
@@ -0,0 +1,19 @@
+<?php
+
+function sanitize($post){
+    foreach ($post as $key => $value){
+        $sanitized_post[$key] = htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+    }
+    
+    return $sanitized_post;
+}
+
+function connectDB() {
+    $dsn = 'mysql:dbname=shop; host=localhost; charset=utf8';
+    $user = 'root';
+    $password = 'root';
+    $dbh = new PDO($dsn, $user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PD0::ERRMODE_EXCEPTION);
+
+    return $dbh;
+}
